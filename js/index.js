32704e9e15eace7be11cb4f304914b00d877394f
Jose Cuevas <josecueram@gmail.com>
2019-09-05 11:20:56 -0500

    Basic Observer implementation

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
-console.log('Hello')
+const $specialBox = document.querySelector('.special.box')
+
+function callback (entries, observer) {
+    console.log('callback called')
+}
+
+const options = {
+    //root,
+    //rootMargin: ,
+    threshold: 0,
+}
+
+
+const observer = new IntersectionObserver(callback, options)
+
+observer.observe($specialBox)
